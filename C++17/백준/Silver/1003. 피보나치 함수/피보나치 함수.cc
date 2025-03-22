dffa8795e14d5a6d3ffe3bfef5e1f7f658015bc4
MokJeongBin <109899411+mokmokahr@users.noreply.github.com>
2025-03-22 16:41:59 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+    int fibo[41];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i < 41; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        if (num >1) {
+            printf("%d %d\n", fibo[num-1], fibo[num]);
+        }
+        else if (num == 1) {
+            printf("0 1\n");
+        }
+        else {
+            printf("1 0\n");
+        }
+
+        
+    }
+
+}
