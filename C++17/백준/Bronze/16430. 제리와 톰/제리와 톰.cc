137f6a0ca4a200ce32af0f6df4811efc1cec746e
MokJeongBin <109899411+mokmokahr@users.noreply.github.com>
2025-03-28 18:14:13 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+
+
+int main(void) {
+	int n, m;
+	cin >> n>>m;
+	cout << m-n << " " << m;
+}
