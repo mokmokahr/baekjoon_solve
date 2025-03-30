76596035d398723a18cd94e1fcd59e86e86e213c
MokJeongBin <109899411+mokmokahr@users.noreply.github.com>
2025-03-31 00:18:28 +0900

    [Silver IV] Title: solved.ac, Time: 80 ms, Memory: 5108 KB -BaekjoonHub

---
+++ C++17/백준/Silver/18110. solved．ac/solved．ac.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+using namespace std;
+
+
+
+int main(void) {
+	vector <int> v;
+	long long n;
+	double res = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int comment;
+		cin >> comment;
+		v.push_back(comment);
+	}
+	sort(v.begin(), v.end());
+
+	int average = round(n * 0.15);
+
+	for (int i = average; i < n - average; i++) {
+		res += v[i];
+	}
+	if(n) cout << static_cast<int>(round(res / (n - (average * 2))));
+	else {
+		cout << 0;
+	}
+
+}
