d999e0f715a1003434d88f828c7f3c2b87c9c23e
MokJeongBin <109899411+mokmokahr@users.noreply.github.com>
2025-09-08 10:00:14 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    while(N--){
+        long long a,b;
+        cin>>a>>b;
+        cout<<(a*a)/(b*b)<<"\n";
+    }
+}
