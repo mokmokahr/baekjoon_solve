62ce536dd246d3020e68c1f63cf12b929bc8fea3
MokJeongBin <109899411+mokmokahr@users.noreply.github.com>
2025-09-28 19:18:02 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int TC = 0;
+int x, y, length, d = 0;
+int move_count = 0;
+
+int main(void) {
+    scanf("%d", &TC);
+    for(int i = 0; i < TC; i++) {
+        scanf("%d %d", &x, &y);
+        length = y - x;
+        d = sqrt(length); // 1. 제곱근 계산
+        pow(d, 2) == length ? move_count = 2 * d - 1 : move_count = 2 * d; // 2. 제곱 수 case
+        if (length > d * (d + 1)) move_count += 1; // 3. 중간 값 case
+        printf("%d\n", move_count);
+    }
+}
