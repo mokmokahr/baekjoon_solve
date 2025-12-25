45403e67c267b0e5561325eb89b60b9fdfabc870
MokJeongBin <109899411+mokmokahr@users.noreply.github.com>
2025-12-25 15:24:33 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    while(N--){
+        int a,b;
+        scanf("%d,%d",&a,&b);
+        cout<<a+b<<"\n";
+    }
+}
