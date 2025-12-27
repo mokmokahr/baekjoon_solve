2929f9ba358b6060b644854dd334d265207e4341
MokJeongBin <109899411+mokmokahr@users.noreply.github.com>
2025-12-28 02:03:51 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<N-1-i; j++) cout<<" ";
+        for(int j = 0; j<2*i+1; j++){
+            if(j == 0 || j == 2*i || i == 0 || i == N-1) cout<<"*";
+            else cout<<" ";
+        }
+        cout<<"\n";
+    }
+}
